01291283d6b064dc4ba7876942ce7c7f69d2a291
Booreung <120548686+Booreung@users.noreply.github.com>
2024-07-24 01:53:29 +0900

    "추가: 매니저 강사 전체 조회 기능
    - 강사 조회 기능 추가
    - 필터링 o ( 교육과정 명으로 조회 )
    - 배정안된 강사 회의 필요(메서드 제작 완료)
    
    "

---
+++ src/main/java/project/homelearn/controller/manager/ManagerTeacherController.java
@@ -0,0 +1,36 @@
+package project.homelearn.controller.manager;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import project.homelearn.dto.manager.ManagerStudentDto;
+import project.homelearn.dto.manager.MangerTeacherDto;
+import project.homelearn.service.manager.ManagerTeacherService;
+
+@Slf4j
+@RestController
+@RequestMapping("/manager")
+@RequiredArgsConstructor
+public class ManagerTeacherController {
+
+    private final ManagerTeacherService managerTeacherService;
+
+    @GetMapping("/manage-teacher")
+    public Page<MangerTeacherDto> teacherList(@RequestParam(name = "page", defaultValue = "0") int page,
+                                              @RequestParam(name = "curriculumName", required = false) String curriculumName){
+
+        Page<MangerTeacherDto> teachers;
+        if(curriculumName != null && !curriculumName.isEmpty()){
+            teachers = managerTeacherService.getTeachersWithCurriculumName(15, page, curriculumName);
+        }
+        else{
+            teachers = managerTeacherService.getTeachers(15, page);
+        }
+
+        return teachers;
+    }
+}
---
+++ src/main/java/project/homelearn/dto/manager/MangerTeacherDto.java
@@ -0,0 +1,28 @@
+package project.homelearn.dto.manager;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MangerTeacherDto {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Long curriculumTh;
+
+    @NotNull
+    private String curriculumName;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    private String email;
+
+//    @NotNull
+//    private boolean isAssigned;
+}
