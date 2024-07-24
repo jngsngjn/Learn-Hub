3d373538f83cb0ac4df77274a5013d4b079fff59
jngsngjn <wjdtjdwls98@gmail.com>
2024-07-24 10:10:25 +0900

    완성 : 학생 1명 등록 기능 완성
    - 추후 엑셀 파일로 대량 등록 기능 추가해야 함

--- src/main/java/project/homelearn/dto/manager/StudentEnrollDto.java
+++ src/main/java/project/homelearn/dto/manager/StudentEnrollDto.java
@@ -20,9 +20,9 @@ public class StudentEnrollDto {
 }
 /*
 {
-  "name": "정성진",
-  "email": "wjdtjdwls98@gmail.com",
-  "phone": "010-3102-6950",
+  "name": "홍길동",
+  "email": "홍길동@gmail.com",
+  "phone": "010-1234-5678",
   "curriculumFullName": "네이버 클라우드 데브옵스 과정 1기"
 }
  */
