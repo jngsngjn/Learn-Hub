f27f84b769257a5a79f1463a92874d8520807fa2
kimsu10 <betnwjd25@gmail.com>
2024-07-29 21:11:31 +0900

    추가 : react  파일 구조 세팅

---
+++ src/main/front/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <>
+      <h1>안녕하세요? Footer입니다</h1>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/main/front/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="nav_container">
+      <h3>안녕하세요? Nav입니다</h3>
+    </div>
+  );
+};
+
+export default Nav;
