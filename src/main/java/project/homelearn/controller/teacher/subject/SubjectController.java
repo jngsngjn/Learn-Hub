a976b9df27a1f98548d5662ff81f0a1c7f593bac
jngsngjn <wjdtjdwls98@gmail.com>
2024-07-30 22:58:54 +0900

    추가 : SubjectController 생성

---
+++ src/main/java/project/homelearn/controller/teacher/subject/SubjectController.java
@@ -0,0 +1,15 @@
+package project.homelearn.controller.teacher.subject;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
+public class SubjectController {
+
+
+}
