81258e0a30425e7d51d43eef7a13412d8c9cbd48
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-01 10:09:30 +0900

    추가 : TeacherCalendarController 생성

--- src/main/java/project/homelearn/controller/teacher/calendar/TeacherCalendarController.java
+++ src/main/java/project/homelearn/controller/teacher/calendar/TeacherCalendarController.java
@@ -2,6 +2,8 @@ package project.homelearn.controller.teacher.calendar;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.homelearn.service.teacher.TeacherCalendarService;
@@ -11,13 +13,17 @@ import project.homelearn.service.teacher.TeacherCalendarService;
  */
 @Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/teachers/calendar")
 @RequiredArgsConstructor
 public class TeacherCalendarController {
 
     private final TeacherCalendarService calendarService;
 
     // 일정 등록
+    @PostMapping
+    public ResponseEntity<?> enrollSchedule() {
+
+    }
 
     // 일정 수정
 
