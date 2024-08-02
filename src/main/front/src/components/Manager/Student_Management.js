070009b9f02d477a0b24a500c60cc213399c9a88
Anseongmin5739 <smahn4069@gmail.com>
2024-08-02 09:19:13 +0900

    수정: 학생 등록 완료 - 이메일 전송까지 가능 표에 저장 필요

--- src/main/front/src/components/Manager/Student_Management.js
+++ src/main/front/src/components/Manager/Student_Management.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import Modal from './Modal';
 import './Student_Management.css';
 
-// 기본 API URL 설정
 axios.defaults.baseURL = 'http://localhost:8080';
 
 const StudentManagement = () => {
@@ -17,6 +16,8 @@ const StudentManagement = () => {
     gender: '',
     email: '',
     phone: '',
+    curriculum: '', // 추가
+    generation: '', // 추가
     curriculumFullName: '',
   });
   const [selectedFile, setSelectedFile] = useState(null); // 선택된 파일
@@ -30,11 +31,9 @@ const StudentManagement = () => {
   const fetchStudents = async () => {
     try {
       const token = getToken();
-      console.log(token);
       const response = await axios.get('/managers/manage-students', {
         headers: { access: token },
       });
-      console.log('응답?', response);
       setStudents(response.data.content || []);
     } catch (error) {
       console.error('응답에러:', error);
@@ -43,7 +42,12 @@ const StudentManagement = () => {
   };
 
   const handleSearch = (event) => setSearchTerm(event.target.value); // 검색어 변경
-  const handleCourseChange = (course) => setSelectedCourse(course); // 교육 과정 변경
+  const handleCourseChange = (course) => {
+    const fullCourseName = course === 'NCP' ? '네이버 클라우드 데브옵스 과정' : 'AWS 데브옵스 과정';
+    setSelectedCourse(course);
+    setNewStudent({ ...newStudent, curriculum: fullCourseName });
+  }; // 교육 과정 변경
+
   const handleGenerationChange = (event) => setSelectedGeneration(event.target.value); // 기수 변경
   const handleRefresh = () => {
     setSearchTerm('');
@@ -70,7 +74,7 @@ const StudentManagement = () => {
         gender: newStudent.gender,
         email: newStudent.email,
         phone: newStudent.phone,
-        curriculumFullName: `${newStudent.curriculum} 데브옵스 과정 ${newStudent.generation}`
+        curriculumFullName: `${newStudent.curriculum} ${newStudent.generation}`
       };
 
       console.log('전송할 학생 데이터:', studentData);
@@ -89,21 +93,22 @@ const StudentManagement = () => {
       console.error('이건 오류메시지:', error);
     }
   };
+
   // 성별 선택 핸들러
   const handleGenderChange = (gender) => {
-    console.log(gender);
     setNewStudent({ ...newStudent, gender: gender === '남' ? 'MALE' : 'FEMALE' });
   };
 
   // 입력 변경
   const handleInputChange = (e) => setNewStudent({ ...newStudent, [e.target.name]: e.target.value });
+
   // 파일 선택
   const handleFileChange = (e) => setSelectedFile(e.target.files[0]);
+
   // 학생 삭제
   const handleDeleteStudent = async () => {
     try {
       const token = getToken();
-      console.log('Token:', token);
       const deletePromises = selectedStudents.map(studentId =>
         axios.delete(`/managers/manage-students/${studentId}`, {
           headers: { access: token },
@@ -116,14 +121,17 @@ const StudentManagement = () => {
       console.error('오류메시지:', error);
     }
   };
+
   // 체크박스 변경
   const handleCheckboxChange = (studentId) => setSelectedStudents(
     selectedStudents.includes(studentId)
       ? selectedStudents.filter(id => id !== studentId)
       : [...selectedStudents, studentId]
   );
+
   // 행 클릭
   const handleRowClick = (studentId) => handleCheckboxChange(studentId);
+
   // 파일 제거
   const handleRemoveFile = () => setSelectedFile(null);
 
@@ -189,60 +197,60 @@ const StudentManagement = () => {
         <button onClick={() => setIsModalOpen(true)}>학생 등록</button>
         <button onClick={handleDeleteStudent}>학생 삭제</button>
       </div>
-       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-            <span className="add_title">학생 등록</span>
-            <div className="form-container">
-              <label className="excel-label">엑셀 파일 첨부 </label>
-              <div className="file-upload">
-                {selectedFile ? (
-                  <div className="file-preview">
-                    <i className="fa-regular fa-file"></i>
-                    <span>{selectedFile.name}</span>
-                    <button className="file-remove-button" onClick={handleRemoveFile}>삭제</button>
-                  </div>
-                ) : (
-                  <>
-                    <label htmlFor="file-upload" className="custom-file-upload">파일 선택</label>
-                    <input id="file-upload" type="file" onChange={handleFileChange} style={{ display: 'none' }} />
-                  </>
-                )}
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <span className="add_title">학생 등록</span>
+        <div className="form-container">
+          <label className="excel-label">엑셀 파일 첨부 </label>
+          <div className="file-upload">
+            {selectedFile ? (
+              <div className="file-preview">
+                <i className="fa-regular fa-file"></i>
+                <span>{selectedFile.name}</span>
+                <button className="file-remove-button" onClick={handleRemoveFile}>삭제</button>
               </div>
-              <div className="course-selection">
-                <button className={`course-button ${newStudent.curriculum === 'NCP' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: 'NCP' })}>NCP</button>
-                <button className={`course-button ${newStudent.curriculum === 'AWS' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: 'AWS' })}>AWS</button>
-              </div>
-              <div className="generation-selection">
-                <select name="generation" value={newStudent.generation} onChange={handleInputChange}>
-                  <option value="1기">1기</option>
-                  <option value="2기">2기</option>
-                  <option value="3기">3기</option>
-                </select>
-              </div>
-              <div className="student-input-group">
-                <label>이름</label>
-                <input type="text" name="name" value={newStudent.name} onChange={handleInputChange} />
-              </div>
-              <div className="gender-selection">
-                <label className="gender-label">성별</label>
-                <div className="gender-buttons">
-                  <button className={`gender-button ${newStudent.gender === 'MALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('남')}>남</button>
-                  <button className={`gender-button ${newStudent.gender === 'FEMALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('여')}>여</button>
-                </div>
-              </div>
-              <div className="student-input-group">
-                <label>이메일</label>
-                <input type="email" name="email" value={newStudent.email} onChange={handleInputChange} />
-              </div>
-              <div className="student-input-group">
-                <label>전화번호</label>
-                <input type="text" name="phone" value={newStudent.phone} onChange={handleInputChange} />
-              </div>
-              <div className="student-modal-actions">
-                <button className="student-modal-button" onClick={handleAddStudent}>학생 등록</button>
-                <button className="student-modal-button" onClick={() => setIsModalOpen(false)}>등록 취소</button>
-              </div>
+            ) : (
+              <>
+                <label htmlFor="file-upload" className="custom-file-upload">파일 선택</label>
+                <input id="file-upload" type="file" onChange={handleFileChange} style={{ display: 'none' }} />
+              </>
+            )}
+          </div>
+          <div className="course-selection">
+            <button className={`course-button ${newStudent.curriculum === '네이버 클라우드 데브옵스 과정' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: '네이버 클라우드 데브옵스 과정' })}>네이버 클라우드 데브옵스 과정</button>
+            <button className={`course-button ${newStudent.curriculum === 'AWS 데브옵스 과정' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: 'AWS 데브옵스 과정' })}>AWS 데브옵스 과정</button>
+          </div>
+          <div className="generation-selection">
+            <select name="generation" value={newStudent.generation} onChange={handleInputChange}>
+              <option value="1기">1기</option>
+              <option value="2기">2기</option>
+              <option value="3기">3기</option>
+            </select>
+          </div>
+          <div className="student-input-group">
+            <label>이름</label>
+            <input type="text" name="name" value={newStudent.name} onChange={handleInputChange} />
+          </div>
+          <div className="gender-selection">
+            <label className="gender-label">성별</label>
+            <div className="gender-buttons">
+              <button className={`gender-button ${newStudent.gender === 'MALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('남')}>남</button>
+              <button className={`gender-button ${newStudent.gender === 'FEMALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('여')}>여</button>
             </div>
-          </Modal>
+          </div>
+          <div className="student-input-group">
+            <label>이메일</label>
+            <input type="email" name="email" value={newStudent.email} onChange={handleInputChange} />
+          </div>
+          <div className="student-input-group">
+            <label>전화번호</label>
+            <input type="text" name="phone" value={newStudent.phone} onChange={handleInputChange} />
+          </div>
+          <div className="student-modal-actions">
+            <button className="student-modal-button" onClick={handleAddStudent}>학생 등록</button>
+            <button className="student-modal-button" onClick={() => setIsModalOpen(false)}>등록 취소</button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
