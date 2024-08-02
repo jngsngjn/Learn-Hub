e0c0b983bf5dcbcade0cdb6651a531ef90b0e144
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-02 13:17:45 +0900

    .

--- src/main/java/project/homelearn/controller/teacher/calendar/TeacherCalendarController.java
+++ src/main/java/project/homelearn/controller/teacher/calendar/TeacherCalendarController.java
@@ -1,12 +1,15 @@
 package project.homelearn.controller.teacher.calendar;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.homelearn.dto.teacher.calendar.TeacherScheduleEnrollDto;
 import project.homelearn.service.teacher.TeacherCalendarService;
+
+import java.security.Principal;
 
 /**
  * Author : 정성진
@@ -21,8 +24,10 @@ public class TeacherCalendarController {
 
     // 일정 등록
     @PostMapping
-    public ResponseEntity<?> enrollSchedule() {
-
+    public ResponseEntity<?> enrollSchedule(Principal principal,
+                                            @Valid @RequestBody TeacherScheduleEnrollDto scheduleDto) {
+        calendarService.addSchedule(principal.getName(), scheduleDto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // 일정 수정
