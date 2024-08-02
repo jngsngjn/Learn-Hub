71be044b9369201675269e154f5fa7b22efd115e
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-02 15:31:17 +0900

    추가 : 강사 대시보드 뉴스 기능 구현

--- src/main/java/project/homelearn/controller/teacher/TeacherDashBoardController.java
+++ src/main/java/project/homelearn/controller/teacher/TeacherDashBoardController.java
@@ -2,8 +2,14 @@ package project.homelearn.controller.teacher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.homelearn.dto.teacher.dashboard.NewsDto;
+import project.homelearn.service.teacher.NewsService;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -11,4 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeacherDashBoardController {
 
+    private final NewsService newsService;
+
+    @GetMapping("/news")
+    public List<NewsDto> viewNews() {
+        try {
+            return newsService.fetchLatestNews();
+        } catch (IOException e) {
+            log.error("Error fetching news", e);
+            return List.of();
+        }
+    }
 }
