99d23b63b7ceab6522e24ecbb34ca8e2dd0f91ba
kimsu10 <betnwjd25@gmail.com>
2024-08-03 18:07:26 +0900

    추가 : 학생 강의 과제 페이지 목데이터 불러오기

--- src/main/front/src/pages/Student/StudentAssignment.js
+++ src/main/front/src/pages/Student/StudentAssignment.js
@@ -1,6 +1,66 @@
+import React, { useEffect, useState } from "react";
 import "./StudentAssignment.css";
+import { useNavigate } from "react-router-dom";
+import useGetFetch from "../../hooks/useGetFetch";
 
 const StudentAssignment = () => {
+  const navigate = useNavigate();
+
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [endAssignmentIdx, setEndAssignmentIdx] = useState(0);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const {
+    data: curAssignment,
+    loading: curAssignmentLoading,
+    error: curAssignmentError,
+  } = useGetFetch("/data/student/mainLecture/currentAssignments.json", []);
+
+  const {
+    data: endAssignments,
+    loading: endAssignmentsLoading,
+    error: endAssignmentsError,
+  } = useGetFetch("/data/student/mainLecture/endAssignments.json", []);
+
+  if (curAssignmentLoading || endAssignmentsLoading) {
+    return <div>데이터를 불러오는 중입니다.</div>;
+  }
+
+  if (curAssignmentError || endAssignmentsError) {
+    return <div>데이터를 불러오는데 오류가 발생했습니다.</div>;
+  }
+
+  const currentAssignment =
+    curAssignment.length > 0 ? curAssignment[currentIdx] : null;
+
+  const handlePrevClick = () => {
+    setCurrentIdx((prev) => (prev === 0 ? curAssignment.length - 1 : prev - 1));
+  };
+
+  const handleNextClick = () => {
+    setCurrentIdx((prev) => (prev === curAssignment.length - 1 ? 0 : prev + 1));
+  };
+
+  const handleEndPrevClick = () => {
+    setEndAssignmentIdx((prev) =>
+      prev === 0 ? endAssignments.length - 1 : prev - 1
+    );
+  };
+
+  const handleEndNextClick = () => {
+    setEndAssignmentIdx((prev) =>
+      prev === endAssignments.length - 1 ? 0 : prev + 1
+    );
+  };
+
+  const curEndAssignments = endAssignments.slice(
+    endAssignmentIdx,
+    endAssignmentIdx + 3
+  );
+
   return (
     <div className="student_subject_body">
       <div className="side_bar">
@@ -14,74 +74,120 @@ const StudentAssignment = () => {
               진행 중인 과제
             </h3>
             <div className="controll_box_with_both_side">
-              <i class="bi bi-chevron-left Right_and_left_button"></i>
-              <i class="bi bi-chevron-right Right_and_left_button"></i>
+              <i
+                className={`bi bi-chevron-left Right_and_left_button ${
+                  currentIdx === 0 ? "disabled" : ""
+                }`}
+                onClick={currentIdx !== 0 ? handlePrevClick : null}
+              ></i>
+              <i
+                className={`bi bi-chevron-right Right_and_left_button ${
+                  currentIdx === curAssignment.length - 1 ? "disabled" : ""
+                }`}
+                onClick={
+                  currentIdx !== curAssignment.length - 1
+                    ? handleNextClick
+                    : null
+                }
+              ></i>
             </div>
           </div>
           {/* 진행중인 과제 */}
-          <div className="current_proceeding_subject_contents_container">
-            <div className="current_proceeding_subject_contents_title_box">
-              <h3 className="current_proceeding_subject_contents_title">
-                Spring으로 게시판 생성
-              </h3>
-              <span className="go_to_proceeding_subject_page">
-                자세히 보기 ⟩
-              </span>
-            </div>
-            <p className="current_proceeding_subject_contents">
-              스프링과 그래들 설정하고 게시판 만들어오세요
-            </p>
-            <div className="current_proceeding_subject_contents_additional_data_box">
-              <p className="">
-                <span className="current_proceeding_subject_contents_deadline geen_text">
-                  2024.08.21
+          {currentAssignment ? (
+            <div className="current_proceeding_subject_contents_container">
+              <div className="current_proceeding_subject_contents_title_box">
+                <h3 className="current_proceeding_subject_contents_title">
+                  {currentAssignment.title}
+                </h3>
+                <span
+                  className="go_to_proceeding_subject_page"
+                  onClick={() =>
+                    navigate(
+                      `/students/assignmentDetail/${currentAssignment.id}`
+                    )
+                  }
+                >
+                  자세히 보기 ⟩
                 </span>
-                &nbsp;까지
+              </div>
+              <p className="current_proceeding_subject_contents">
+                {currentAssignment.content}
               </p>
-              <p className="current_proceeding_subject_contents_Participants">
-                <span className="current_proceeding_subject_contents_Participants_count geen_text">
-                  28명
-                </span>
-                &nbsp; 제출
-              </p>
+              <div className="current_proceeding_subject_contents_additional_data_box">
+                <p className="">
+                  <span className="current_proceeding_subject_contents_deadline geen_text">
+                    {currentAssignment.deadline}
+                  </span>
+                  &nbsp;까지
+                </p>
+                <p className="current_proceeding_subject_contents_Participants">
+                  <span className="current_proceeding_subject_contents_Participants_count geen_text">
+                    {currentAssignment.participants}
+                  </span>
+                  &nbsp;명 제출
+                </p>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div>진행 중인 과제가 없습니다.</div>
+          )}
         </div>
         {/* 마감된 과제 */}
         <div className="closed_subject_container">
           <div className="closed_subject_title_box">
-            <h3 className="closed_subject_box_name">진행 중인 과제</h3>
+            <h3 className="closed_subject_box_name">마감된 과제</h3>
             <div className="controll_box_with_both_side">
-              <i class="bi bi-chevron-left Right_and_left_button"></i>
-              <i class="bi bi-chevron-right Right_and_left_button"></i>
+              <i
+                className={`bi bi-chevron-left Right_and_left_button ${
+                  endAssignmentIdx === 0 ? "disabled" : ""
+                }`}
+                onClick={endAssignmentIdx !== 0 ? handleEndPrevClick : null}
+              ></i>
+              <i
+                className={`bi bi-chevron-right Right_and_left_button ${
+                  endAssignmentIdx >= endAssignments.length - 3
+                    ? "disabled"
+                    : ""
+                }`}
+                onClick={
+                  endAssignmentIdx < endAssignments.length - 3
+                    ? handleEndNextClick
+                    : null
+                }
+              ></i>
             </div>
           </div>
           {/* 마감된 과제 리스트 */}
-          <div className="closed_subject_contents_container">
-            <div className="closed_subject_contents_title_box">
-              <h3 className="closed_subject_contents_title">
-                Spring으로 게시판 생성
-              </h3>
-              <span className="go_to_closed_subject_page">자세히 보기 ⟩</span>
-            </div>
-            <p className="closed_subject_contents">
-              스프링과 그래들 설정하고 게시판 만들어오세요
-            </p>
-            <div className="closed_subject_contents_additional_data_box">
-              <p className="">
-                <span className="closed_subject_contents_deadline geen_text">
-                  2024.08.21
+          {curEndAssignments.map((el, idx) => (
+            <div className="closed_subject_contents_container" key={idx}>
+              <div className="closed_subject_contents_title_box">
+                <h3 className="closed_subject_contents_title">{el.title}</h3>
+                <span
+                  className="go_to_closed_subject_page"
+                  onClick={() =>
+                    navigate(`/students/assignmentDetail/${el.id}`)
+                  }
+                >
+                  자세히 보기 ⟩
                 </span>
-                &nbsp;까지
-              </p>
-              <p className="closed_subject_contents_Participants">
-                <span className="closed_subject_contents_Participants_count geen_text">
-                  28명
-                </span>
-                &nbsp; 제출
-              </p>
+              </div>
+              <p className="closed_subject_contents">{el.content}</p>
+              <div className="closed_subject_contents_additional_data_box">
+                <p className="">
+                  <span className="closed_subject_contents_deadline geen_text">
+                    {el.deadline}
+                  </span>
+                  &nbsp;까지
+                </p>
+                <p className="closed_subject_contents_Participants">
+                  <span className="closed_subject_contents_Participants_count geen_text">
+                    {el.participants}
+                  </span>
+                  &nbsp;명 제출
+                </p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
