7823961ff63b13c9c3c2fa5f97990c8ce893aaf7
kimsu10 <betnwjd25@gmail.com>
2024-08-04 17:12:51 +0900

    수정 : 과제 상세 페이지 토클 css

--- src/main/front/src/pages/Student/StudentAssignmentDetail.js
+++ src/main/front/src/pages/Student/StudentAssignmentDetail.js
@@ -86,7 +86,7 @@ const StudentAssignmentDetail = () => {
         </div>
         <div className="subject_board_lists_container">
           {assignments?.participants?.map((participant, idx) => (
-            <div key={participant.studentId}>
+            <div key={participant.studentId} className="subject_board_list_one">
               <div className="subject_submitted_lists">
                 <p className="submitted_number">{idx + 1}</p>
                 <p className="submitted_name">{participant.name}</p>
