356bbc29371a7fe426dbb60b86bb823b09cd005f
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-06 23:54:31 +0900

    추가 : 학생 설문조사 참여 기능 완성

--- src/main/java/project/homelearn/controller/student/survey/StudentSurveyController.java
+++ src/main/java/project/homelearn/controller/student/survey/StudentSurveyController.java
@@ -3,12 +3,12 @@ package project.homelearn.controller.student.survey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.homelearn.dto.student.survey.SurveyModalDto;
 import project.homelearn.service.student.StudentSurveyService;
+
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * Author : 정성진
@@ -25,11 +25,30 @@ public class StudentSurveyController {
     public ResponseEntity<?> viewSurveyModal(@PathVariable("surveyId") Long surveyId) {
         SurveyModalDto result = surveyService.getSurveyModal(surveyId);
         if (result == null) {
-            return new ResponseEntity<>("이미 마감된 설문입니다.",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("이미 마감된 설문입니다.", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 설문 참여 시 : 알림 없애기, 마감된 설문이면 막기
-
+    /*
+    {
+        "1": "5",
+        "2": "4",
+        "3": "5",
+        "4": "4",
+        "5": "3",
+        "6": "훈련장려금이 빨리 들어왔으면 좋겠어요."
+    }
+     */
+    // 설문 참여
+    @PostMapping
+    public ResponseEntity<?> participateSurvey(@PathVariable("surveyId") Long surveyId,
+                                               @RequestBody Map<Long, String> surveyResponses,
+                                               Principal principal) {
+        boolean result = surveyService.participateSurvey(surveyId, surveyResponses, principal.getName());
+        if (result) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
