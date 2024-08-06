68d34f0fce81f6d52341a3c68f72442af006a7de
kimsu10 <betnwjd25@gmail.com>
2024-08-06 11:44:25 +0900

    추가 : 사이드 바

--- src/main/front/src/pages/Student/StudentDashBoard.js
+++ src/main/front/src/pages/Student/StudentDashBoard.js
@@ -1,12 +1,150 @@
+import React, { useState } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
 import "./StudentDashBoard.css";
+import StudentMain from "./StudentMain";
+import StudentLecture from "./StudentLecture";
+import StudentAssignment from "./StudentAssignment";
+import useGetFetch from "../../hooks/useGetFetch";
+import StudentLectureList from "./StudentLectureList";
+import StudentFreeBoard from "./StudentFreeBoard";
+import StudentQuestionBoard from "./StudentQuestionBoard";
+import StudentAssignmentDetail from "./StudentAssignmentDetail";
 
 const StudentDashBoard = () => {
+  const [showSection, setShowSection] = useState(null);
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const navigate = useNavigate();
+
+  const toggleOpen = (section) => {
+    setShowSection(showSection === section ? null : section);
+  };
+
+  const handleSubjectClick = (subjectName) => {
+    setSelectedSubject(subjectName);
+    navigate(`/students/${subjectName}/board`);
+  };
+
+  const handleSectionClick = (section) => {
+    navigate(`/students/${section}`);
+    setShowSection(null);
+  };
+
+  const { data: subject, error: subjecError } = useGetFetch(
+    "/data/student/mainpage/sidebar.json",
+    []
+  );
+
   return (
-    <div className="student_dashboard_body">
-      <div className="side_bar">
-        <h3>옆 카테고리 컴포넌트 자리</h3>
+    <div className="student_dashboard_body" id="container">
+      <div className="sidebar">
+        <div className="profile_box">프로필 컴포넌트가 들어올 자리</div>
+        <h3 className="sidebar_title" onClick={() => handleSectionClick("/")}>
+          대쉬보드
+        </h3>
+        <ul className="student_dashboard_list_container" id="inner_box">
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "subject" ? "show" : ""
+            }`}
+            onClick={() => toggleOpen("subject")}
+          >
+            과목
+            <i className="bi bi-caret-right-fill stu_sidebar_toogle_btn"></i>
+            <i className="bi bi-caret-down-fill stu_sidebar_toogle_btn"></i>
+            <i className="bi bi-caret-left-fill stu_sidebar_toogle_btn"></i>
+            {subject.subject?.map((el, idx) => (
+              <li key={idx} onClick={() => handleSubjectClick(el.name)}>
+                {el.name}
+              </li>
+            ))}
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "assignment" ? "show" : ""
+            }`}
+            onClick={() => handleSectionClick("assignment")}
+          >
+            과제
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "lecture" ? "show" : ""
+            }`}
+            onClick={() => handleSectionClick("lectureLists")}
+          >
+            강의
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "board" ? "show" : ""
+            }`}
+            onClick={() => toggleOpen("board")}
+          >
+            게시판
+            <li onClick={() => handleSectionClick("freeBoard")}>자유 게시판</li>
+            <li onClick={() => handleSectionClick("questionBoard")}>
+              질문 게시판
+            </li>
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "notice" ? "show" : ""
+            }`}
+            onClick={() => toggleOpen("notice")}
+          >
+            공지사항
+            <li onClick={() => handleSectionClick("teacherNotice")}>
+              강사 공지사항
+            </li>
+            <li onClick={() => handleSectionClick("managerNotice")}>
+              매니저 공지사항
+            </li>
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "question" ? "show" : ""
+            }`}
+            onClick={() => toggleOpen("question")}
+          >
+            문의
+            <li onClick={() => handleSectionClick("teacherQuestion")}>
+              강사 문의
+            </li>
+            <li onClick={() => handleSectionClick("managerQuestion")}>
+              매니저 문의
+            </li>
+          </ul>
+          <ul
+            className={`student_dashboard_section ${
+              showSection === "vote" ? "show" : ""
+            }`}
+            onClick={() => handleSectionClick("vote")}
+          >
+            투표
+          </ul>
+        </ul>
       </div>
-      <div className="student_dashboard_components"></div>
+      <div className="contents">
+        <Routes>
+          <Route path="/" element={<StudentMain />} />
+          <Route
+            path=":subjectName/board"
+            element={<StudentLecture subject={selectedSubject} />}
+          />
+          <Route path="assignment" element={<StudentAssignment />} />
+          <Route path="/lectureLists" element={<StudentLectureList />} />
+          <Route path="/freeBoard" element={<StudentFreeBoard />} />
+          <Route path="/questionBoard" element={<StudentQuestionBoard />} />
+          {/* 아래의 페이지가 강사가 봐야할 학생들의 과제제출 페이지 */}
+          <Route path="/teacherNotice" element={<StudentAssignmentDetail />} />
+          {/* 언젠가 들어올 강사 공지사항 페이지 */}
+          {/* <Route path="/teacherNotice" element={< />} /> */}
+          {/* 언젠가 들어올 매니저 공지사항 페이지 */}
+          {/* <Route path="/teacherNotice" element={< />} /> */}
+          {/* 언젠가 들어올 투표 페이지 */}
+          {/* <Route path="/teacherNotice" element={< />} /> */}
+        </Routes>
+      </div>
     </div>
   );
 };
