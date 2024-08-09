f9c9f570ec5b51de39db69755c95a0ba87a3a3f3
kimsu10 <betnwjd25@gmail.com>
2024-08-09 15:35:54 +0900

    에러 발생 : 경로 에러 빠진거

---
+++ src/main/front/src/pages/Manager/CurriculumManagement.js
@@ -0,0 +1,287 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { CirclePicker } from "react-color";
+import axios from "../../utils/axios";
+import "./CurriculumManagement.css";
+import "../Modal/ManagerModal/ManagerModal.css";
+import swal from "sweetalert";
+
+const CurriculumManagement = () => {
+  const [ncpCurriculums, setNcpCurriculums] = useState([]);
+  const [awsCurriculums, setAwsCurriculums] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+  const [newCurriculum, setNewCurriculum] = useState({
+    type: "",
+    startDate: "",
+    endDate: "",
+    color: "",
+    teacherId: "",
+  });
+
+  // 토큰을 가져오기
+  const getToken = () => localStorage.getItem("access-token");
+
+  // 입력 값 변경 시
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewCurriculum({ ...newCurriculum, [name]: value });
+  };
+
+  // 교육 과정 변경 시
+  const handleCourseChange = (courseName) => {
+    setNewCurriculum({ ...newCurriculum, type: courseName });
+  };
+
+  // 색상 선택
+  const handleColorChange = (color) => {
+    setNewCurriculum({ ...newCurriculum, color: color.hex });
+    setIsColorPickerOpen(false);
+  };
+
+  // 과정 추가
+  const handleAddCurriculum = async () => {
+    const newCurriculumItem = {
+      type: newCurriculum.type,
+      startDate: newCurriculum.startDate,
+      endDate: newCurriculum.endDate,
+      color: newCurriculum.color,
+      teacherId: newCurriculum.teacherId,
+    };
+
+    // 종료일이 시작일보다 빠르거나 같으면 swal 메시지
+    if (
+      new Date(newCurriculumItem.endDate) <=
+      new Date(newCurriculumItem.startDate)
+    ) {
+      swal("등록 실패", "종료일은 시작일 이후여야 합니다.", "warning");
+      return;
+    }
+
+    try {
+      const token = getToken();
+      const response = await axios.post(
+        "/managers/manage-curriculums/enroll",
+        newCurriculumItem,
+        {
+          headers: { access: token },
+        }
+      );
+
+      if (response.status === 200) {
+        setIsModalOpen(false);
+        fetchCurriculums(newCurriculum.type); // 목록 갱신
+      } else {
+        console.error("교육 과정 등록 실패");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // NCP AWS 교육 과정 목록 가져오기
+  const fetchCurriculums = async (type) => {
+    try {
+      const token = getToken();
+      const response = await axios.get(`/managers/manage-curriculums/${type}`, {
+        headers: { access: token },
+      });
+
+      if (type === "NCP") {
+        setNcpCurriculums(response.data || []);
+      } else if (type === "AWS") {
+        setAwsCurriculums(response.data || []);
+      }
+    } catch (error) {
+      console.error("에러 메시지:", error);
+    }
+  };
+
+  // 과정 목록을 가져옴
+  useEffect(() => {
+    fetchCurriculums("NCP");
+    fetchCurriculums("AWS");
+  }, []);
+
+  // 과정 목록 렌더링
+  const renderCurriculumList = (curriculums) =>
+    curriculums.map((curriculum) => (
+      <div key={curriculum.id} className="curriculum-card">
+        <div className="curriculum-header">
+          <span
+            className="curriculum-th"
+            style={{ backgroundColor: curriculum.color }}
+          >
+            {curriculum.th}기
+          </span>
+          <Link
+            to={`/managers/manage-curriculums/${curriculum.id}`}
+            className="curriculum-type"
+          >
+            {curriculum.name}
+          </Link>
+        </div>
+        <div className="curriculum-footer">
+          <span className="curriculum-teacher">
+            강사 {curriculum.teacherName}
+          </span>
+          <span className="curriculum-students">
+            <i className="fas fa-user"></i> {curriculum.attendance} /{" "}
+            {curriculum.total}
+          </span>
+        </div>
+      </div>
+    ));
+
+  return (
+    <div className="curriculum-management">
+      <h1>교육 과정</h1>
+      <div className="button-container">
+        <button
+          className="curriculum-add-button"
+          onClick={() => setIsModalOpen(true)}
+        >
+          교육 과정 추가
+        </button>
+      </div>
+      <div className="curriculum-container">
+        <div className="curriculum-column">
+          <h2>NCP 과정</h2>
+          <div className="curriculum-list">
+            {renderCurriculumList(ncpCurriculums)}
+          </div>
+        </div>
+        <div className="curriculum-column">
+          <h2>AWS 과정</h2>
+          <div className="curriculum-list">
+            {renderCurriculumList(awsCurriculums)}
+          </div>
+        </div>
+      </div>
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button
+              className="modal-close"
+              onClick={() => setIsModalOpen(false)}
+            >
+              ×
+            </button>
+            <span className="curriculum-submit">교육 과정 등록</span>
+            <div className="course-selection">
+              <button
+                className={`course-button ${
+                  newCurriculum.type === "NCP" ? "selected" : ""
+                }`}
+                onClick={() => handleCourseChange("NCP")}
+              >
+                NCP
+              </button>
+              <button
+                className={`course-button ${
+                  newCurriculum.type === "AWS" ? "selected" : ""
+                }`}
+                onClick={() => handleCourseChange("AWS")}
+              >
+                AWS
+              </button>
+            </div>
+            <div className="curriculum-input-group">
+              <label>시작일</label>
+              <input
+                className="curriculum-start-date-input"
+                type="date"
+                name="startDate"
+                value={newCurriculum.startDate}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="curriculum-input-group">
+              <label>종료일</label>
+              <input
+                className="curriculum-end-date-input"
+                type="date"
+                name="endDate"
+                value={newCurriculum.endDate}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="curriculum-input-group">
+              <label>기수 색상</label>
+              <div className="color-input-wrapper">
+                <input
+                  className="color-input"
+                  type="text"
+                  name="color"
+                  value={newCurriculum.color}
+                  readOnly
+                />
+                <div
+                  className="color-input-select"
+                  onClick={() => setIsColorPickerOpen(true)}
+                >
+                  <div
+                    className="color-box"
+                    style={{ backgroundColor: newCurriculum.color }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            {isColorPickerOpen && (
+              <div
+                className="color-picker-modal-overlay"
+                onClick={() => setIsColorPickerOpen(false)}
+              >
+                <div
+                  className="color-picker-modal-content"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <CirclePicker
+                    color={newCurriculum.color}
+                    onChangeComplete={handleColorChange}
+                    colors={[
+                      "#F3C41E",
+                      "#F58D11",
+                      "#B85B27",
+                      "#A90C57",
+                      "#F45CE5",
+                      "#AE59F0",
+                      "#0A8735",
+                      "#6F961E",
+                      "#19E308",
+                      "#1D1AA6",
+                      "#20CFF5",
+                      "#98B3E5",
+                    ]}
+                  />
+                </div>
+              </div>
+            )}
+            <div className="curriculum-input-group">
+              <label>강사</label>
+              <input
+                type="text"
+                name="teacherId"
+                value={newCurriculum.teacherId}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="modal-actions">
+              <button className="modal-button" onClick={handleAddCurriculum}>
+                교육 과정 등록
+              </button>
+              <button
+                className="modal-button"
+                onClick={() => setIsModalOpen(false)}
+              >
+                등록 취소
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CurriculumManagement;
---
+++ src/main/front/src/pages/Manager/StudentManagement.js
@@ -0,0 +1,336 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../../utils/axios';
+import ManagerModal from '../Modal/ManagerModal/ManagerModal';
+import ProgressModal from './ProgressModal';
+import './Student_Management.css';
+import swal from 'sweetalert';
+
+const StudentManagement = () => {
+  const [students, setStudents] = useState([]);
+  const [curriculums, setCurriculums] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCourse, setSelectedCourse] = useState('전체');
+  const [selectedGeneration, setSelectedGeneration] = useState('전체');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newStudent, setNewStudent] = useState({
+    name: '',
+    gender: '',
+    email: '',
+    phone: '',
+    curriculum: '',
+    generation: '',
+    curriculumFullName: '',
+  });
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isProgressModalOpen, setIsProgressModalOpen] = useState(false);
+  const [selectedStudents, setSelectedStudents] = useState([]);
+  const getToken = () => localStorage.getItem('access-token');
+
+  useEffect(() => {
+    fetchStudents();
+    fetchCurriculums();
+  }, []);
+
+  const fetchStudents = async () => {
+    try {
+      const response = await axios.get('/managers/manage-students');
+      if (response.data && response.data.content) {
+        setStudents(response.data.content);
+      } else {
+        setStudents([]);
+      }
+    } catch (error) {
+      console.error('응답 에러:', error);
+      setStudents([]);
+    }
+  };
+
+  const fetchCurriculums = async () => {
+    try {
+      const response = await axios.get('/managers/enroll-user-ready');
+      if (response.data) {
+        console.log(response.data);
+        setCurriculums(response.data);
+      } else {
+        setCurriculums([]);
+      }
+    } catch (error) {
+      console.error('기수 가져오기 에러:', error);
+      setCurriculums([]);
+    }
+  };
+
+  const handleSearch = (event) => setSearchTerm(event.target.value);
+
+  const handleCourseChange = (course) => {
+    const fullCourseName = course === 'NCP' ? '네이버 클라우드 데브옵스 과정' : 'AWS 데브옵스 과정';
+    setSelectedCourse(fullCourseName);
+    setSelectedGeneration('전체');
+    setNewStudent({ ...newStudent, curriculum: fullCourseName });
+  };
+
+  const handleGenerationChange = (event) => setSelectedGeneration(event.target.value);
+
+  const handleRefresh = () => {
+    setSearchTerm('');
+    setSelectedCourse('전체');
+    setSelectedGeneration('전체');
+  };
+
+  const filteredStudents = students.filter(student =>
+    student.name.includes(searchTerm) &&
+    (selectedCourse === '전체' || student.curriculumName === selectedCourse) &&
+    (selectedGeneration === '전체' || student.curriculumTh === parseInt(selectedGeneration))
+  );
+
+  const handleAddStudent = async () => {
+    try {
+      const token = getToken();
+
+      const generation = newStudent.generation ? parseInt(newStudent.generation) : 1;
+      const curriculumFullName = `${newStudent.curriculum} ${generation}기`;
+
+      const studentData = {
+        name: newStudent.name,
+        gender: newStudent.gender,
+        email: newStudent.email,
+        phone: newStudent.phone,
+        curriculumFullName: curriculumFullName,
+      };
+
+      console.log('등록할 학생 데이터:', studentData);
+      console.log('curriculumFullName:', curriculumFullName);
+
+      const response = await axios.post('/managers/manage-students/enroll', studentData, {
+        headers: { access: token },
+      });
+      console.log('학생 등록 응답:', response.data);
+      if (response.status === 200) {
+        setIsModalOpen(false);
+        fetchStudents();
+      } else {
+        console.error('학생 등록 실패');
+      }
+    } catch (error) {
+      console.error('이건 오류메시지:', error);
+    }
+  };
+
+
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      console.log('파일이 선택되지 않았습니다.');
+      return;
+    }
+    try {
+      const token = getToken();
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      setIsProgressModalOpen(true);
+
+      const response = await axios.post('/managers/manage-students/enroll-file', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'access': token,
+        },
+      });
+      console.log('파일 업로드 응답:', response.data);
+      setSelectedFile(null);
+      setIsProgressModalOpen(false);
+
+      swal({
+        title: "업로드 성공",
+        text: "파일 업로드가 완료되었습니다.",
+        icon: "success",
+        button: "확인",
+      });
+    } catch (error) {
+      console.error('파일 업로드 에러:', error);
+      console.error('에러 상세 정보:', error.response ? error.response.data : '응답 없음');
+      setUploadProgress(0);
+      setIsProgressModalOpen(false);
+
+      swal({
+        title: "업로드 실패",
+        text: "파일 업로드 중 오류가 발생했습니다.",
+        icon: "error",
+        button: "확인",
+      });
+    }
+  };
+
+  const handleGenderChange = (gender) => {
+    setNewStudent({ ...newStudent, gender: gender === '남' ? 'MALE' : 'FEMALE' });
+  };
+
+  const handleInputChange = (e) => setNewStudent({ ...newStudent, [e.target.name]: e.target.value });
+
+  const handleFileChange = (e) => setSelectedFile(e.target.files[0]);
+
+  const handleDeleteStudent = async () => {
+    try {
+      const token = getToken();
+      const deletePromises = selectedStudents.map(studentId =>
+        axios.delete(`/managers/manage-students/${studentId}`, {
+          headers: { access: token },
+        })
+      );
+      await Promise.all(deletePromises);
+      console.log('학생 삭제 응답:', deletePromises);
+      fetchStudents();
+      setSelectedStudents([]);
+    } catch (error) {
+      console.error('삭제 에러:', error);
+    }
+  };
+
+  const handleCheckboxChange = (studentId) => setSelectedStudents(
+    selectedStudents.includes(studentId)
+      ? selectedStudents.filter(id => id !== studentId)
+      : [...selectedStudents, studentId]
+  );
+
+  const handleRowClick = (studentId) => handleCheckboxChange(studentId);
+
+  const handleRemoveFile = () => {
+    setSelectedFile(null);
+    setUploadProgress(0);
+  };
+
+  const filteredGenerations = curriculums.find(curriculum => curriculum.type === (selectedCourse === '네이버 클라우드 데브옵스 과정' ? 'NCP' : 'AWS'))?.th || [];
+
+  return (
+    <div className="student-management">
+      <h1>학생 관리</h1>
+      <div className="student-controls">
+        <div className="student-program-buttons">
+          <button className={selectedCourse === '네이버 클라우드 데브옵스 과정' ? 'selected' : ''} onClick={() => handleCourseChange('NCP')}>NCP</button>
+          <button className={selectedCourse === 'AWS 데브옵스 과정' ? 'selected' : ''} onClick={() => handleCourseChange('AWS')}>AWS</button>
+          <select value={selectedGeneration} onChange={handleGenerationChange}>
+            <option value="전체">전체</option>
+            {filteredGenerations.map(th => (
+              <option key={`${th}`} value={th}>{`${th}기`}</option>
+            ))}
+          </select>
+        </div>
+        <div className="student-search-container">
+          <div className="student-search-wrapper">
+            <input type="text" placeholder="검색" value={searchTerm} onChange={handleSearch} />
+            <i className="fas fa-search student-search-icon"></i>
+          </div>
+          <button onClick={handleRefresh} className="student-refresh-button">
+            <i className="fas fa-sync"></i>
+          </button>
+        </div>
+      </div>
+      <div className="student-table-container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>번호</th>
+              <th>교육 과정명</th>
+              <th>기수</th>
+              <th>이름</th>
+              <th>성별</th>
+              <th>이메일</th>
+              <th>전화번호</th>
+              <th>출석 여부</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredStudents.length > 0 ? (
+              filteredStudents.map((student, index) => (
+                <tr key={index} onClick={() => handleRowClick(student.studentId)} className={selectedStudents.includes(student.studentId) ? 'selected' : ''}>
+                  <td>
+                    <input type="checkbox" checked={selectedStudents.includes(student.studentId)} onChange={() => handleCheckboxChange(student.studentId)} onClick={(e) => e.stopPropagation()} />
+                  </td>
+                  <td>{index+1}</td>
+                  <td>{student.curriculumName}</td>
+                  <td>{student.curriculumTh}</td>
+                  <td>{student.name}</td>
+                  <td>{student.gender === 'MALE' ? '남' : '여'}</td>
+                  <td>{student.email}</td>
+                  <td>{student.phone}</td>
+                  <td>{student.isAttend ? <span className="status present">✔</span> : <span className="status absent">✘</span>}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="9">등록된 학생이 없습니다</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="student-actions">
+        <button onClick={() => setIsModalOpen(true)}>학생 등록</button>
+        <button onClick={handleDeleteStudent}>학생 삭제</button>
+      </div>
+      <ManagerModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <span className="add_title">학생 등록</span>
+        <div className="student-form-container">
+          <label className="student-excel-label">엑셀 파일 첨부 </label>
+          <div className="student-file-upload">
+            {selectedFile ? (
+              <div className="student-file-preview">
+                <i className="fa-regular fa-file"></i>
+                <span>{selectedFile.name}</span>
+                <button className="student-file-remove-button" onClick={handleRemoveFile}>삭제</button>
+              </div>
+            ) : (
+              <>
+                <label htmlFor="file-upload" className="student-custom-file-upload">파일 선택</label>
+                <input id="file-upload" type="file" onChange={handleFileChange} style={{ display: 'none' }} />
+              </>
+            )}
+          </div>
+          <div className="student-modal-file">
+            <button className="student-modal-file-button" onClick={handleFileUpload}>파일 업로드</button>
+            <button className="student-modal-button" onClick={() => setIsModalOpen(false)}>등록 취소</button>
+          </div>
+          <span className="student-file-line" />
+          <div className="student-course-selection">
+            <button className={`student-course-button ${newStudent.curriculum === '네이버 클라우드 데브옵스 과정' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: '네이버 클라우드 데브옵스 과정' })}>NCP</button>
+            <button className={`student-course-button ${newStudent.curriculum === 'AWS 데브옵스 과정' ? 'selected' : ''}`} onClick={() => setNewStudent({ ...newStudent, curriculum: 'AWS 데브옵스 과정' })}>AWS</button>
+          </div>
+          <div className="student-generation-selection">
+            <select name="generation" value={newStudent.generation} onChange={handleInputChange}>
+              {(newStudent.curriculum === '네이버 클라우드 데브옵스 과정' ? curriculums.find(curriculum => curriculum.type === 'NCP')?.th : curriculums.find(curriculum => curriculum.type === 'AWS')?.th || []).map(th => (
+                <option key={`${th}`} value={th}>{`${th}기`}</option>
+              ))}
+            </select>
+          </div>
+          <div className="student-input-group">
+            <label>이름</label>
+            <input type="text" name="name" value={newStudent.name} onChange={handleInputChange} />
+          </div>
+          <div className="student-gender-selection">
+            <label className="student-gender-label">성별</label>
+            <div className="student-gender-buttons">
+              <button className={`student-gender-button ${newStudent.gender === 'MALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('남')}>남</button>
+              <button className={`student-gender-button ${newStudent.gender === 'FEMALE' ? 'selected' : ''}`} onClick={() => handleGenderChange('여')}>여</button>
+            </div>
+          </div>
+          <div className="student-input-group">
+            <label>이메일</label>
+            <input type="email" name="email" value={newStudent.email} onChange={handleInputChange} />
+          </div>
+          <div className="student-input-group">
+            <label>전화번호</label>
+            <input type="text" name="phone" value={newStudent.phone} onChange={handleInputChange} />
+          </div>
+          <div className="student-modal-actions">
+            <button className="student-modal-button" onClick={handleAddStudent}>학생 등록</button>
+            <button className="student-modal-button" onClick={() => setIsModalOpen(false)}>등록 취소</button>
+          </div>
+        </div>
+      </ManagerModal>
+      <ProgressModal isOpen={isProgressModalOpen} onClose={() => setIsProgressModalOpen(false)} setUploadProgress={setUploadProgress} />
+    </div>
+  );
+};
+
+export default StudentManagement;
