937f2fd8b9586c07f53aa639af04d4e1af682641
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-09 16:14:52 +0900

    수정 : 배지 데이터 추가

--- src/main/java/project/homelearn/entity/student/badge/Badge.java
+++ src/main/java/project/homelearn/entity/student/badge/Badge.java
@@ -2,10 +2,12 @@ package project.homelearn.entity.student.badge;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Badge {
 
     @Id
@@ -23,4 +25,11 @@ public class Badge {
 
     @Column(name = "image_path", nullable = false)
     private String imagePath;
+
+    public Badge(String name, String description, String imageName, String imagePath) {
+        this.name = name;
+        this.description = description;
+        this.imageName = imageName;
+        this.imagePath = imagePath;
+    }
 }
