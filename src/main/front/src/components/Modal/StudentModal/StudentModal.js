55db18d97e166959b77e5b6c6e3f3a88e0b10af2
kimsu10 <betnwjd25@gmail.com>
2024-08-10 18:43:07 +0900

    추가 : 학생 자유게시판 스타일

--- src/main/front/src/components/Modal/StudentModal/StudentModal.js
+++ src/main/front/src/components/Modal/StudentModal/StudentModal.js
@@ -13,59 +13,59 @@ const StudentModal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="modal show">
-      <div className="modal_content">
+    <div className="student_modal show">
+      <div className="student_modal_content">
         <span className="close" onClick={closeModal}>
           &times;
         </span>
-        <h1 className="modal_title">과제 제출</h1>
-        <form onSubmit={handleSubmit} className="modal_form_body">
+        <h1 className="student_modal_title">과제 제출</h1>
+        <form onSubmit={handleSubmit} className="student_modal_form_body">
           <label>
-            <p className="modal_name_tag">제목</p>
+            <p className="student_modal_name_tag">제목</p>
             <input
               type="text"
               name="title"
               value={formData.title}
               onChange={handleChange}
-              className="modal_input_title"
+              className="student_modal_input_title"
             />
           </label>
           <label>
-            <p className="modal_content_tag">내용</p>
+            <p className="student_modal_content_tag">내용</p>
             <textarea
               name="content"
               value={formData.content}
               onChange={handleChange}
-              className="modal_input_content"
+              className="student_modal_input_content"
             ></textarea>
           </label>
-          <label className="modal_file_label">
-            <p className="modal_file_tag">파일 첨부</p>
-            <div className="modal_file_input_wrapper">
+          <label className="student_modal_file_label">
+            <p className="student_modal_file_tag">파일 첨부</p>
+            <div className="student_modal_file_input_wrapper">
               <input
                 type="text"
                 readOnly
                 value={selectedFileName}
-                className="modal_input_file_display"
+                className="student_modal_input_file_display"
               />
-              <label className="modal_file_button">
+              <label className="student_modal_file_button">
                 파일 선택
                 <input
                   type="file"
                   name="file"
                   onChange={handleFileChange}
-                  className="modal_input_file"
+                  className="student_modal_input_file"
                 />
               </label>
             </div>
           </label>
-          <div className="modal_submit_button_box">
-            <button type="submit" className="modal_submit_button">
+          <div className="student_modal_submit_button_box">
+            <button type="submit" className="student_modal_submit_button">
               과제 제출
             </button>
             <button
               type="button"
-              className="modal_cancel_button"
+              className="student_modal_cancel_button"
               onClick={closeModal}
             >
               제출 취소
--- src/main/front/src/pages/Student/StudentFreeBoard.js
+++ src/main/front/src/pages/Student/StudentFreeBoard.js
@@ -1,5 +1,77 @@
+import { useState } from "react";
+import StudentModal from "../../components/Modal/StudentModal/StudentModal";
+import "./StudentFreeBoard.css";
+
 const StudentFreeBoard = () => {
-  return <>학생 자유 게시판</>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    file: null,
+  });
+  const [selectedFileName, setSelectedFileName] = useState("");
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "file") {
+      setFormData((prevState) => ({ ...prevState, [name]: files[0] }));
+    } else {
+      setFormData((prevState) => ({ ...prevState, [name]: value }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("모달 데이터 : " + formData);
+    closeModal();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFileName(file.name);
+      handleChange(e);
+    }
+  };
+  return (
+    <div className="main_container">
+      <div className="student_freeboard_page_title_box">
+        <h1 className="student_freeboard_page_title">자유 게시판</h1>
+        <button className="regist_freeboard_writing" onClick={openModal}>
+          글 등록
+        </button>
+      </div>
+      {/* 자유 게시판 글 목록 */}
+      <table className="student_freeboard_table">
+        <tr className="student_freeboard_table_header">
+          <th>번호</th>
+          <th>제목</th>
+          <th>작성자</th>
+          <th>작성일</th>
+          <th>답변 수</th>
+        </tr>
+        <tr className="student_freeboard_table_body">
+          <th>1</th>
+          <th>다들 수업 잘 따라 갈 수 있나요</th>
+          <th>김수정</th>
+          <th>2024-08-07</th>
+          <th>3</th>
+        </tr>
+      </table>
+      <StudentModal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        formData={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleFileChange={handleFileChange}
+        selectedFileName={formData.selectedFileName}
+      />
+    </div>
+  );
 };
 
 export default StudentFreeBoard;
