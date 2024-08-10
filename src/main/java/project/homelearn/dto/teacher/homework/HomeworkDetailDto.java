cd46821c362dad18a417fec78c28508d9518c698
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-10 21:12:52 +0900

    추가 : 강사 과제 상세 조회 시 변경사항 추가

--- src/main/java/project/homelearn/dto/teacher/homework/HomeworkDetailDto.java
+++ src/main/java/project/homelearn/dto/teacher/homework/HomeworkDetailDto.java
@@ -3,6 +3,7 @@ package project.homelearn.dto.teacher.homework;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
+import project.homelearn.entity.homework.AcceptFile;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -18,6 +19,8 @@ public class HomeworkDetailDto {
     private String fileName;
     private String filePath;
     private LocalDate enrollDate;
+    private Boolean requiredFile;
+    private AcceptFile acceptFile;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime deadLine;
@@ -33,6 +36,8 @@ public class HomeworkDetailDto {
     "fileName": null,
     "filePath": null,
     "enrollDate": "2024-08-03",
+    "requiredFile": true,
+    "acceptFile": "JAVA",
     "deadLine": "2024-08-01 10:15",
     "unsubmittedCount": 1,
     "unsubmittedList": [
