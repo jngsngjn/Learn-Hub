816b97610535bb2ca4ee3ff75f8a2f20831d2f0e
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-11 17:51:07 +0900

    추가 : CompilerController

---
+++ src/main/java/project/homelearn/controller/student/lecture/CompilerController.java
@@ -0,0 +1,16 @@
+package project.homelearn.controller.student.lecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author : 정성진
+ */
+@RestController
+@RequestMapping("/students/compile")
+@RequiredArgsConstructor
+public class CompilerController {
+
+
+}
