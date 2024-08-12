33d6d2ef90d783e4c011c62a280217d64bfde2ae
Noh Seung Been <shtmdqls990921@gmail.com>
2024-08-12 09:57:55 +0900

    추가 : MainPage 세팅

--- src/main/front/src/index.js
+++ src/main/front/src/index.js
@@ -4,6 +4,7 @@ import Router from "./Router";
 import "./styles/reset.css";
 import "./styles/base.css";
 import "./styles/global.css";
+import "swiper/css/bundle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Router />);
