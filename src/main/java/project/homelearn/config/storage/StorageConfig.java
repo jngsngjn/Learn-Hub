d36a739323d090d044f9f0729b6a37dc329af891
jngsngjn <wjdtjdwls98@gmail.com>
2024-08-14 19:34:52 +0900

    추가 : StorageConfig 로그 추가

--- src/main/java/project/homelearn/config/storage/StorageConfig.java
+++ src/main/java/project/homelearn/config/storage/StorageConfig.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import static project.homelearn.config.storage.StorageConstants.NCP_STORAGE_URL;
 import static project.homelearn.config.storage.StorageConstants.REGION_NAME;
 
+@Slf4j
 @Configuration
 public class StorageConfig {
 
@@ -24,6 +26,8 @@ public class StorageConfig {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
+        log.info("accessKey = {}", accessKey);
+        log.info("secretKey = {}", secretKey);
         BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
         ClientConfiguration clientConfig = new ClientConfiguration();
         clientConfig.setSignerOverride("AWSS3V4SignerType"); // 명시적으로 Signer 설정
